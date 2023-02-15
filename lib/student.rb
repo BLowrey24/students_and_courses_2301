5f1537e694bf9ec7d4371873b9fbe7b3ca5e6d64
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-15 11:18:23 -0500

    feat: #grade returns the averge of the grades in scores

--- lib/student.rb
+++ lib/student.rb
@@ -10,4 +10,14 @@ class Student
   def log_score(score)
     @scores << score
   end
+
+  def grade
+    if @scores.empty?
+      return nil
+    else
+      total = @scores.sum
+      average = total.to_f / @scores.length
+      return average.round(1)
+    end
+  end
 end
