13c3adf78c692314cf9e046b6562015e898a840c
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-15 11:49:54 -0500

    add test: for #add_course in gradebook spec

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -5,6 +5,8 @@ require './lib/student'
 RSpec.describe Gradebook do
 
   let(:gradebook) { Gradebook.new("Mr. Lowrey") }
+  let(:course1) { Course.new("Calculus", 2) }
+  let(:course2) { Course.new("Science", 4) }
 
   describe '#initialize' do
     it 'exists' do
@@ -14,6 +16,18 @@ RSpec.describe Gradebook do
     it 'has attributes' do
       expect(gradebook.instructor).to eq("Mr. Lowrey")
       expect(gradebook.courses).to eq([])
+    end
+  end
+
+  describe '#add_course' do
+    it 'add courses to the courses array' do
+      gradebook.add_course(course1)
+      
+      expect(gradebook.courses).to eq([course1])
+      
+      gradebook.add_course(course2)
+      
+      expect(gradebook.courses).to eq([course1, course2])
     end
   end
 end
