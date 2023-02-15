c16e3bfadcdddd4ef9ce237b8e996e49ae00f4f0
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-15 11:46:18 -0500

    add tests: for the class gradebook in its spec

---
+++ spec/gradebook_spec.rb
@@ -0,0 +1,18 @@
+require './lib/gradebook'
+require './lib/course'
+require './lib/student'
+
+RSpec.describe Gradebook do
+
+  let(:gradebook) { Gradebook.new("Mr. Lowrey") }
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(gradebook).to be_a Gradebook
+    end
+
+    it 'has attributes' do
+      expect(gradebook.instructor).to eq("Mr. Lowrey")
+      expect(gradebook.courses).to eq([])
+    end
+  end
