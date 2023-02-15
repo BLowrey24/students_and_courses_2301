6226862c59b8645659617598d892ef950ca7ff0a
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-15 11:31:59 -0500

    add test: for #enroll in course spec

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -4,6 +4,8 @@ require './lib/student'
 RSpec.describe Course do
 
   let(:course) { Course.new("Calculus", 2) }
+  let(:student1) { Student.new({name: "Morgan", age: 21}) }
+  let(:student2) { Student.new({name: "Jordan", age: 29}) }
 
   describe '#initialize' do
     it 'exists' do
@@ -20,6 +22,16 @@ RSpec.describe Course do
   describe '#full?' do
     it 'shows if the course is full or not' do
       expect(course.full?).to be false
+    end
+  end
+
+  describe '#enroll' do
+    it 'can add a student to the students array' do
+      course.enroll(student1)
+      course.enroll(student2)
+
+      expect(course.students).to eq ([student1, student2])
+      expect(course.full?).to be true
     end
   end
 end
